5a2a00d477ee1c76bbbcae44f4a840f2bc528108
Mauricio Matus F <mauricio@Mauricios-MacBook-Pro.local>
2022-08-24 12:39:26 -0400

    corrección de error en parametros, ahora agranda y exporta en ProRes 422 y 444. Falta incorporar opciones de filtro reduccion de ruido

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ ffmpeg.setFfmpegPath(ffmpegPath);
 
 
 //Obtenemos Metadata
+/*
 ffmpeg.ffprobe('/Volumes/SSD_02/Desarrollo_ProcVideo/Footage/noche.avi', (data, err)=> {
   if (err) {
     console.log(err)
@@ -11,29 +12,33 @@ ffmpeg.ffprobe('/Volumes/SSD_02/Desarrollo_ProcVideo/Footage/noche.avi', (data, 
     console.log(data)
   }
 });
+*/
 
 //Probamos procesar tamaño video
 ffmpeg()
-  .input('/Volumes/SSD_02/Desarrollo_ProcVideo/Footage/noche.avi')
+  .input('/Volumes/SSD_02/Desarrollo_ProcVideo/Footage/video.avi')
   //.inputFormat('avi')
   .FPSOutput(24)
   .size('1920x1080')
-  .videoCodec('libx264rgb')
-  .videoBitrate('30000k')
-  //.videoCodec('prores')
+  //.videoCodec('libx264rgb')
+  //.videoBitrate('30000k')
+  .videoCodec('prores_ks')
   
-  .addOptions(['-pix_fmt yuv420p'])
+  //.addOptions(['-pix_fmt yuv422p10le', '-profile:v 3'])
+  .addOptions(['-pix_fmt yuva444p10le', '-profile:v 3'])
+  
   .videoFilters(
-    {
-      filter: 'atadenoiser',
-      //options: { a:0.02, b:0.04, a:0.02, b:15, a:0.02, b:0.02, s:9, p:'all', a:'p', s:32767  }
-    })
-  .toFormat('mp4')
+      {
+        filter: 'atadenoise',
+        //options: { a:0.02, b:0.04, a:0.02, b:15, a:0.02, b:0.02, s:9, p:'all', a:'p', s:32767  }
+      }
+    ) 
+  .toFormat('mov')
   .on('progress', function(progress) {
     console.log('Progreso ' + Object.keys(progress));
     console.log('Progreso ' + progress.frames + progress);
   })
-  .output('/Volumes/SSD_02/Desarrollo_ProcVideo/Footage_salida/noche-salida.mp4')
+  .output('/Volumes/SSD_02/Desarrollo_ProcVideo/Footage_salida/noche-salidav2.mov')
   .on('error', function(err) {
   console.log(err);
   })
